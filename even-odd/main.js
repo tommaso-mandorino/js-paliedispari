869c944aab9876693fd736edb5f2981a5a8bed0b
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-03 23:25:41 +0200

    feat(even-odd exercise): add checking even/odd fuction declaration

--- even-odd/main.js
+++ even-odd/main.js
@@ -4,3 +4,21 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 
 }
+
+// Checking if number is even or odd function declaration
+function isEvenOrOdd(number) {
+
+    // IF number divided by 2 has no remainder
+    if (number % 2 === 0) {
+
+        // Return number is even
+        return 'even';
+
+    } else {
+
+        // Return number is odd
+        return 'odd';
+
+    }
+
+}
